84582a13b095df3b879b617cea7b12d83c13d3db
Ravinath Fernando <ravinath_fernando@combank.net>
2023-01-25 15:16:38 +0530

    Reading query string parameters

--- src/main/java/com/devz/app/ws/controller/UserController.java
+++ src/main/java/com/devz/app/ws/controller/UserController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users") // http://localhost:8080/users
 public class UserController {
 
+    @GetMapping
+    public String getUsers(@RequestParam(value = "page") int page,@RequestParam( value = "limit") int limit){
+        return "get users was called page = "+page;
+    }
+
+
     @GetMapping(path = "/{userId}")
     public String getUser(@PathVariable String userId){
         return "get user was called with user id = "+userId;
