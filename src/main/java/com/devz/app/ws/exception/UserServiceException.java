edef8b1cffa523225715c7438cf59226d3e84588
Ravinath Fernando <ravinath_fernando@combank.net>
2023-02-01 14:13:01 +0530

    user defined exception

---
+++ src/main/java/com/devz/app/ws/exception/UserServiceException.java
@@ -0,0 +1,10 @@
+package com.devz.app.ws.exception;
+
+public class UserServiceException extends RuntimeException {
+
+//    public static final long serialVersionUID = 1212124545454L;
+
+    public UserServiceException(String message){
+        super(message);
+    }
+}
