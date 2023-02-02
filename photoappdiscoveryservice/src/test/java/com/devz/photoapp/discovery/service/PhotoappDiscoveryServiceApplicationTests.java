db3d7f3f4547e22215d4d7172df4769d4fd699d1
Ravinath Fernando <ravinath_fernando@combank.net>
2023-02-02 14:45:17 +0530

    42 add user service

---
+++ photoappdiscoveryservice/src/test/java/com/devz/photoapp/discovery/service/PhotoappDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devz.photoapp.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ photouser-service/src/main/java/userservice/photouserservice/PhotouserServiceApplication.java
@@ -0,0 +1,13 @@
+package userservice.photouserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotouserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotouserServiceApplication.class, args);
+	}
+
+}
