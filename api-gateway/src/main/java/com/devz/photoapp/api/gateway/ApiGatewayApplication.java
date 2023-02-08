01da46f077fc63883ee6f2c40c3a6b8659edee7f
Ravinath Fernando <ravinath_fernando@combank.net>
2023-02-08 16:45:47 +0530

    62. Automatic Mapping of Gateway Routes

--- api-gateway/src/main/java/com/devz/photoapp/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/devz/photoapp/api/gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.devz.photoapp.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
