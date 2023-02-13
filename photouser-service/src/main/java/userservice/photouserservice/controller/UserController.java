debf1a02348b9f25c48fa67416f9249427043b62
Ravinath Fernando <ravinath_fernando@combank.net>
2023-02-13 16:14:29 +0530

    78. Validating HTTP Request Body

--- photouser-service/src/main/java/userservice/photouserservice/controller/UserController.java
+++ photouser-service/src/main/java/userservice/photouserservice/controller/UserController.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import userservice.photouserservice.data.UserEntity;
 import userservice.photouserservice.model.CreateUserRequestModel;
@@ -27,12 +29,12 @@ public class UserController {
     }
 
     @PostMapping
-    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails){
+    public ResponseEntity createUser(@Valid @RequestBody CreateUserRequestModel userDetails){
 
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserDto userDto  = modelMapper.map(userDetails,UserDto.class);
         userService.createUser(userDto);
-        return "create user method calling";
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
