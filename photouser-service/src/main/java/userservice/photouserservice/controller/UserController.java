88ce098359e16149e84d41d0663e5e82ab7cf9bc
Ravinath Fernando <ravinath_fernando@combank.net>
2023-02-13 09:37:36 +0530

    77. Implementing the Create User Request Model class

--- photouser-service/src/main/java/userservice/photouserservice/controller/UserController.java
+++ photouser-service/src/main/java/userservice/photouserservice/controller/UserController.java
@@ -1,15 +1,23 @@
 package userservice.photouserservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+import userservice.photouserservice.model.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private Environment env;
 
     @GetMapping("/status/check")
     public String status(){
-        return  "working";
+        return  "working on port "+env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody CreateUserRequestModel userDetails){
+        return "create user method calling";
     }
 }
